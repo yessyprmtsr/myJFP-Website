336f973577b6c08bcbe9739a12448d84a53edf46
yessyprmtsr <52453800+yessyprmtsr@users.noreply.github.com>
2020-12-09 10:27:34 +0700

    transaction (not fixed)

--- myJFPApplication/app/Http/Controllers/Customer/BookingController.php
+++ myJFPApplication/app/Http/Controllers/Customer/BookingController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use App\Models\Transactions;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
@@ -16,7 +18,7 @@ class BookingController extends Controller
     {
         return view('customer.book');
     }
-
+    
     /**
      * Show the form for creating a new resource.
      *
@@ -35,7 +37,50 @@ class BookingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+         //validasi
+         $this->validate($request,[
+            'sender_name' => 'required',
+            'sender_phone_number' => 'required',
+            'good_name' => 'required',
+            'good_type' => 'required',
+            'good_weight' => 'required',
+            'service' => 'required',
+            'payment' => 'required',
+            'cover' => 'file|image',
+            'date' => 'required',
+            'time' => 'required',
+            'sender_address' => 'required',
+            'receiver_name' => 'required',
+            'receiver_phone_number' => 'required',
+            'receiver_address' => 'required',  
+        ]);
+        $booking = new Transactions();
+        $booking->sender_name = $request->input('sender_name');
+        $booking->sender_phone_number = $request->input('sender_phone_number');
+        $booking->good_name = $request->input('good_name');
+        $booking->good_type = $request->input('good_type');
+        $booking->good_weight = $request->input('good_weight');
+        $booking->service = $request->input('service');
+        $booking->payment = $request->input('payment');
+        if($request->hasFile('cover')){
+            $file = $request->file('cover');
+            $extension = $file->getClientOriginalExtension();
+            $filename = time() . "." . $extension;
+            $file->move('uploads/image/',$filename);
+            $booking->cover = $filename;
+        } else {
+            return $request;
+            $booking->cover = "";
+        }
+        $booking->date = $request->input('date');
+        $booking->time = $request->input('time');
+        $booking->sender_address = $request->input('sender_address');
+        $booking->receiver_name = $request->input('receiver_name');
+        $booking->receiver_phone_number = $request->input('receiver_phone_number');
+        $booking->receiver_address = $request->input('receiver_address');
+        $booking->total = $request('total');
+        $booking->save();
+        return redirect()->route('home');
     }
 
     /**
--- myJFPApplication/resources/views/customer/book.blade.php
+++ myJFPApplication/resources/views/customer/book.blade.php
@@ -1,36 +1,39 @@
 @extends('customer.template.layout')
 @section('content')
+
 <div class="container">
-    <form action="" method="post">
+<form action="kk.html" method="POST" enctype="multipart/form-data" method="POST" enctype="multipart/form-data">
+    @csrf
         <h1 style="text-align: center;margin-top:10px">Booking Data</h1>
         <div class="row" style="margin-top:30px">
             <div class="col-lg-6">
                 <h2>Data Pengirim</h2>
                 <div class="form-group">
                     <label for="exampleFormControlInput1">Nama Pengirim</label>
-                    <input type="text" name="sender_name" class="form-control" id="exampleFormControlInput1">
+                    <input required type="text" name="sender_name" class="form-control" id="exampleFormControlInput1">
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlInput1">Nomor Telefon</label>
-                    <input type="number" name="sender_phone_number" class="form-control" id="exampleFormControlInput1">
+                    <input required type="number" name="sender_phone_number" class="form-control" id="exampleFormControlInput1">
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlInput1">Nama Barang</label>
-                    <input type="text" name="good_name" class="form-control" id="exampleFormControlInput1">
+                    <input required type="text" name="good_name" class="form-control" id="exampleFormControlInput1">
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlInput1">Jenis Barang</label>
-                    <input type="text" name="good_type" class="form-control" id="exampleFormControlInput1">
+                    <input required type="text" name="good_type" class="form-control" id="exampleFormControlInput1">
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlInput1">Berat Barang</label>
-                    <input type="number" name="good_weight" class="form-control" id="exampleFormControlInput1">
+                    <input required type="number" name="good_weight" class="form-control" id="good_weight">
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlSelect1">Jenis Service</label>
-                    <select name="service" class="form-control" id="exampleFormControlSelect1">
+                    <select name="service" class="form-control" id="service" onchange="calculateAmount(this.value)">
+                        <option value="0">--Choose Service--</option>
                         <option value="OKE">OKE</option>
-                        <option value="REGULER">REGULER</option>
+                        <option value="REGULER" >REGULER</option>
                     </select>
                 </div>
                 <div class="form-group">
@@ -58,7 +61,7 @@
                 
                 <div class="form-group">
                     <label for="exampleFormControlTextarea1">Alamat Pengirim</label>
-                    <textarea name="sender_address" class="form-control" id="exampleFormControlTextarea1"
+                    <textarea required name="sender_address" class="form-control" id="exampleFormControlTextarea1"
                         rows="3"></textarea>
                 </div>
             </div>
@@ -66,15 +69,15 @@
                 <h2>Data Penerima</h2>
                 <div class="form-group">
                     <label for="exampleFormControlInput1">Nama</label>
-                    <input type="text" name="receiver_name" class="form-control" id="exampleFormControlInput1">
+                    <input required type="text" name="receiver_name" class="form-control" id="exampleFormControlInput1">
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlInput1">Nomor Telefon</label>
-                    <input type="number" name="receiver_phone_number" class="form-control" id="exampleFormControlInput1">
+                    <input required type="number" name="receiver_phone_number" class="form-control" id="exampleFormControlInput1">
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlTextarea1">Alamat Penerima</label>
-                    <textarea name="receiver_address" class="form-control" id="exampleFormControlTextarea1"
+                    <textarea required name="receiver_address" class="form-control" id="exampleFormControlTextarea1"
                         rows="3"></textarea>
                 </div>
                 <div class="form-group">
@@ -82,23 +85,13 @@
                         <label for="exampleFormControlInput1">Bukti Transfer</label>
                         <input type="file" name="cover" class="form-control" id="exampleFormControlInput1" >
                 </div>
-                
-                <div class="row">
-                    <div class="col-lg-10">
                         <div class="form-group">
                             <label for="exampleFormControlInput1">Total</label>
-                            <input type="number" name="total" class="form-control" id="exampleFormControlInput1"
-                                disabled>
-                            <input type="hidden" name="total" class="form-control" id="exampleFormControlInput1">
+                            <input type="number" name="total" class="form-control" id="total" disabled
+                                >
+                            <input type="hidden" name="total" class="form-control" id="total">
                         </div>
                     </div>
-                    <div class="col-lg-2">
-                        <div class="form-group">
-                            <a class="btn btn-info" href="{{ route('booking.index')}}" name="count"
-                                style="margin-top: 30px">Count</a>
-                        </div>
-                    </div>
-                </div>
                 <div class="form-group">
                     <input type="submit" name="submit" value="Booking" style="margin-left: 200px" class="btn btn-info">
                 </div>
@@ -117,4 +110,26 @@
         }
 }
     </script>
+    <script>
+        function calculateAmount(val) {
+                var value2 = parseFloat($('#good_weight').val()) || 0;
+                if(val == 'OKE'){
+                   var service = 15000 * value2;
+                } else {
+                    var service = 20000 * value2;
+                }
+                // var service = val * value2;
+                // /*display the result*/
+                var divobj = document.getElementById('total');
+                divobj.value = service;
+            }
+    //  $(function(){
+    //         $('#value1, #good_weight').keyup(function(){
+    //            var value1 = parseFloat($('#value1').val()) || 0;
+    //            var value2 = parseFloat($('#good_weight').val()) || 0;
+    //            $('#total').val(value1 * value2);
+    //         });
+    //      });
+    </script>
+
 @endsection
